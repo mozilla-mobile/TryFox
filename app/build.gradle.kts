e43ef88fdde028596b601b74579cc33933a30296
Titouan Thibaud <tthibaud@mozilla.com>
2025-10-03 18:11:21 +0200

    Use maintained action for release creation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,8 @@ android {
                     }
                 }
             }
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro",
