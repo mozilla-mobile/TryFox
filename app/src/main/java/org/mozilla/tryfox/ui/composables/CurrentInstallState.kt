f6fcf4cbe28774903fbe56b0b43d93a4d4ff0903
Titouan Thibaud <tthibaud@mozilla.com>
2025-10-06 17:46:56 +0200

    Remove the "x days behind" chip on the app cards, as they are currently not accurate.

--- app/src/main/java/org/mozilla/tryfox/ui/composables/CurrentInstallState.kt
+++ app/src/main/java/org/mozilla/tryfox/ui/composables/CurrentInstallState.kt
@@ -4,32 +4,21 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material3.AssistChip
 import androidx.compose.material3.AssistChipDefaults
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import org.mozilla.tryfox.model.AppState
-import org.mozilla.tryfox.util.parseDateToMillis
-import kotlin.time.Duration.Companion.milliseconds
 
 @Composable
 fun CurrentInstallState(
     appState: AppState?,
-    apkDisplayDateString: String,
     modifier: Modifier = Modifier,
 ) {
-    val apkDateMillis: Long = remember(apkDisplayDateString) {
-        parseDateToMillis(apkDisplayDateString)
-    }
-
     Row(
         modifier = modifier.padding(bottom = 8.dp),
         verticalAlignment = Alignment.CenterVertically,
@@ -46,50 +35,11 @@ fun CurrentInstallState(
                     border = null,
                 )
             }
-            apkDateMillis == 0L || appState.installDateMillis == null -> {
+            else -> {
                 AssistChip(
                     onClick = { /* No action */ },
                     label = { Text("Installed") },
                     border = AssistChipDefaults.assistChipBorder(true),
-                )
-            }
-            appState.installDateMillis >= apkDateMillis -> {
-                AssistChip(
-                    onClick = { /* No action */ },
-                    label = { Text("Up to date") },
-                    colors = AssistChipDefaults.assistChipColors(
-                        containerColor = MaterialTheme.colorScheme.primaryContainer,
-                        labelColor = MaterialTheme.colorScheme.onPrimaryContainer,
-                    ),
-                    border = null,
-                )
-            }
-            else -> {
-                val diff = (apkDateMillis - appState.installDateMillis).milliseconds
-                val daysBehind = diff.inWholeDays
-                val hoursBehind = diff.inWholeHours
-
-                val timeBehindText = when {
-                    daysBehind > 31 -> "${daysBehind / 7} weeks behind"
-                    daysBehind > 0 -> "$daysBehind days behind"
-                    hoursBehind > 0 -> "$hoursBehind hours behind"
-                    else -> "Less than an hour behind"
-                }
-                AssistChip(
-                    onClick = { /* No action */ },
-                    label = { Text(timeBehindText) },
-                    leadingIcon = {
-                        Icon(
-                            Icons.Filled.Warning,
-                            contentDescription = "Warning",
-                            tint = MaterialTheme.colorScheme.onTertiaryContainer,
-                        )
-                    },
-                    colors = AssistChipDefaults.assistChipColors(
-                        containerColor = MaterialTheme.colorScheme.tertiaryContainer,
-                        labelColor = MaterialTheme.colorScheme.onTertiaryContainer,
-                    ),
-                    border = null,
                 )
             }
         }
