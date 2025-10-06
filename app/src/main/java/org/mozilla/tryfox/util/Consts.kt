def384f66327a4ca95f64fcb0a5ec2c76feb13b4
Titouan Thibaud <tthibaud@mozilla.com>
2025-10-06 17:34:38 +0200

    Reflect the app installed state after installing an app

--- app/src/main/java/org/mozilla/tryfox/util/Consts.kt
+++ app/src/main/java/org/mozilla/tryfox/util/Consts.kt
@@ -5,3 +5,7 @@ const val FENIX_NIGHTLY: String = "fenix-nightly"
 const val FOCUS: String = "focus"
 const val REFERENCE_BROWSER = "reference-browser"
 const val TREEHERDER = "treeherder"
+
+const val FENIX_PACKAGE = "org.mozilla.fenix"
+const val FOCUS_PACKAGE = "org.mozilla.focus.nightly"
+const val REFERENCE_BROWSER_PACKAGE = "org.mozilla.reference.browser"
