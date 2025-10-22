591c46163670895e6255b1499af93ddce387a04b
Titouan Thibaud <tthibaud@mozilla.com>
2025-10-22 11:37:09 -0400

    Update TryFox app icon

--- app/src/main/java/org/mozilla/tryfox/ui/composables/ArchiveGroupCard.kt
+++ app/src/main/java/org/mozilla/tryfox/ui/composables/ArchiveGroupCard.kt
@@ -42,7 +42,6 @@ import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import kotlinx.datetime.Clock
 import kotlinx.datetime.Instant
 import kotlinx.datetime.LocalDate
