4800be787cec57289ed1392f3506147f9bfbe80e
Titouan Thibaud <tthibaud@mozilla.com>
2025-10-25 11:23:47 -0400

    Add support for Pull to Refresh on the Home screen.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -107,6 +107,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.material)
     implementation(libs.androidx.compose.material3.adaptive.navigation.suite)
 
     // Navigation Compose
