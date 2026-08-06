d6727419d5e532267d17744f0fe222040269d35a
Titouan Thibaud <tthibaud@mozilla.com>
2026-08-06 20:30:36 +0200

    Avoid loading debug build metadata

--- app/src/main/java/org/mozilla/tryfox/ui/screens/HomeAppCard.kt
+++ app/src/main/java/org/mozilla/tryfox/ui/screens/HomeAppCard.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -173,7 +174,9 @@ internal fun HomeAppCard(
                 }
             }
 
-            if (!isDebug || app.installedTryBuild != null) {
+            if (isDebug && app.installedTryBuild != null) {
+                Spacer(modifier = Modifier.height(12.dp))
+            } else if (!isDebug) {
                 Row(
                     modifier = Modifier.fillMaxWidth().padding(top = 12.dp),
                     verticalAlignment = Alignment.Top,
