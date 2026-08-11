180e5c779eeafc08fe305d023675759b1908faa6
Titouan Thibaud <tthibaud@mozilla.com>
2026-08-11 19:00:31 +0200

    Show uninstall conflicts before APK install

--- app/src/main/java/org/mozilla/tryfox/install/InstallState.kt
+++ app/src/main/java/org/mozilla/tryfox/install/InstallState.kt
@@ -3,10 +3,23 @@ package org.mozilla.tryfox.install
 sealed interface InstallState {
     data object Idle : InstallState
     data object Installing : InstallState
-    data class Conflict(val packageName: String) : InstallState
+    data class Conflict(
+        val apps: List<ConflictApp>,
+        val reason: ConflictReason,
+    ) : InstallState
     data object Uninstalling : InstallState
     data class Installed(val packageName: String) : InstallState
     data class Failed(val message: String) : InstallState
+}
+
+data class ConflictApp(
+    val label: String,
+    val packageName: String,
+)
+
+enum class ConflictReason {
+    INCOMPATIBLE_OR_NEWER,
+    SHARED_USER_SIGNATURE,
 }
 
 data class UninstallRequest(
